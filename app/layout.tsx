6970b025c4e1ce3094fbfc5f08729a4cd518d994
Axel Zwaans <axelzwaans@hotmail.com>
2023-09-24 08:12:01 +0100

    Feat: Add header and footer components

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Metadata, ResolvedMetadata } from "next";
 import { Nunito, Nunito_Sans } from "next/font/google";
 import clsx from "clsx";
 import { createClient } from "@/prismicio";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -19,13 +21,15 @@ const nunitoSans = Nunito_Sans({
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
 
-  const page = await client.getSingle("settings");
+  const settings = await client.getSingle("settings");
 
   return {
-    title: page.data.site_title || "Flowrise",
-    description: page.data.meta_description || "Flowrise is the relaxing app for your mind",
+    title: settings.data.site_title || "Flowrise",
+    description:
+      settings.data.meta_description ||
+      "Flowrise is the relaxing app for your mind",
     openGraph: {
-      images: [page.data.og_image.url || ""],
+      images: [settings.data.og_image.url || ""],
     },
   };
 }
@@ -38,9 +42,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={clsx(nunito.variable, nunitoSans.variable)}>
       <body>
-        <header>Header!</header>
+        <Header />
         {children}
-        <footer>Footer!</footer>
+        <Footer />
       </body>
     </html>
   );
