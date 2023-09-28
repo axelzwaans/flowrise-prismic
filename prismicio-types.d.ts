5dd8df89cc3b430d962e0828ad6c6499e9875f21
Axel Zwaans <axelzwaans@hotmail.com>
2023-09-28 11:01:29 +0100

    Feat: Add slices to homepage type

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -5,6 +5,8 @@ import type * as prismic from "@prismicio/client";
 type Simplify<T> = { [KeyType in keyof T]: T[KeyType] };
 
 type HomepageDocumentDataSlicesSlice =
+  | CallToActionSlice
+  | TextWithImageSlice
   | FeaturesSlice
   | TestimonialsSlice
   | HeroSlice;
